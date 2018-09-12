4c214339b72f7afde5f04df43149ef777bc82485
clericus <k.s.pisarenko@gmail.com>
2018-09-12 10:18:06 +0300

    no comment

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  var arr = preferences;
+  var count = 0;
+  for (var i=0; i<arr.length; i++)
+  {
+    for(var j=0; j<arr.length;j++)
+    {
+      if (arr[i]===j+1)
+        {
+          for(var k = 0;k<arr.length;k++)
+            {if(arr[j]===k+1&&arr[k]===i+1)
+              {
+                count=count+1;
+              }
+            }
+        } 
+    } 
+  }
+var result = Math.floor(count/3);
+return result;
+}
